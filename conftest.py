13e609c88e14caa391af9c5484498de48c9fa0e8
ekim22 <ekim2@ggc.edu>
2021-09-28 18:12:00 -0400

    Changed sample_files to sample_docs

--- conftest.py
+++ conftest.py
@@ -3,57 +3,35 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os
 import glob
+import sys
+
+"""
+pytest-xdist doesn't produce live logs without redirecting
+stdout to stderr, even when log_cli is set to true. Running
+pytest without -n (therefore vanilla pytest) produces live logs
+appropriately, so it is only when running xdist this is needed.#!/usr/bin/env python
+
+Refer to this issue for further updates:
+https://github.com/pytest-dev/pytest-xdist/issues/402
+"""
+sys.stdout = sys.stderr
 
 
 def pytest_addoption(parser):
     parser.addoption("--server", action="store", default="localhost:8000")
 
 
-def get_asset_paths():
-    filenames = []
-    import logging
-
-    # logging.warning(os.getcwd())
-    for filename in glob.glob(os.getcwd() + "/assets/*"):
-        filenames.append(filename)
-        # logging.warning(filename)
-    for f in filenames:
-        logging.warn("fileNAME: " + f)
-    logging.warn("filenames: ", filenames)
+def pytest_generate_tests(metafunc):
+    sample_docs = []
+    for sample_doc in glob.glob(os.getcwd() + "/assets/documents/*"):
+        sample_docs.append(sample_doc)
+    if "sample_doc" in metafunc.fixturenames:
+        metafunc.parametrize("sample_doc", sample_docs)
 
 
 @pytest.fixture(scope="session")
 def server(request):
     return "http://" + request.config.getoption("--server")
-
-
-@pytest.fixture(scope="module")
-def authenticate(server):
-    opts = webdriver.ChromeOptions()
-    opts.headless = False
-    opts.add_argument("start-maximized")
-    cd = webdriver.Chrome(options=opts)
-    cd.get(server + "/login")
-    username = "test@test.com"
-    password = "123456"
-
-    username_field = cd.find_element_by_xpath("//*[@id='emailTxt']")
-    username_field.send_keys(username)
-    password_field = cd.find_element_by_xpath("//*[@id='passwordTxt']")
-    password_field.send_keys(password)
-    password_field.send_keys(Keys.RETURN)
-
-
-@pytest.fixture(scope="class")
-def init_chrome_driver(request):
-    opts = webdriver.ChromeOptions()
-    opts.headless = False
-    opts.add_argument("start-maximized")
-    opts.add_argument("user-data-dir=selenium")
-    web_driver = webdriver.Chrome(options=opts)
-    request.cls.driver = web_driver
-    yield
-    web_driver.close()
 
 
 @pytest.fixture(scope="session")
@@ -63,6 +41,7 @@ def init_driver(server, request):
         "download.default_directory": os.getcwd() + "/",
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
+        "plugins.always_open_pdf_externally": True,
     }
     opts.add_experimental_option("prefs", prefs)
     opts.headless = False
@@ -81,16 +60,5 @@ def init_driver(server, request):
     for item in session.items:
         cls = item.getparent(pytest.Class)
         setattr(cls.obj, "driver", web_driver)
-    yield
-    get_asset_paths()
-    web_driver.close()
-
-
-@pytest.fixture(scope="class")
-def init_firefox_driver(request):
-    opts = webdriver.FirefoxOptions()
-    opts.headless = False
-    web_driver = webdriver.Firefox(options=opts)
-    request.cls.driver = web_driver
     yield
     web_driver.close()
--- test_documents/test_documents.py
+++ test_documents/test_documents.py
@@ -7,22 +7,31 @@ import os
 
 @pytest.mark.usefixtures("init_driver")
 class Test_Documents:
-    def test_document_ops(self, server):
+    def test_document_ops(self, server, sample_doc):
         # TODO: create tempfile with extension .docx and upload using sel
         # TODO: make tempdir, download file to temp dir, check the right
         # dl folder for existence of file to pass test.
         # TODO: delete file from app; assert file is gone.
-        self.driver.get(server + "/home")
-        self._doc_upload()
-        # assert self.driver.find_element_by_xpath("//*[@id='documentTableOnHomePage']/div[1]/table/tbody/tr/td[2]").is_displayed()
-        self.driver.get(server + "/home")
-        self._doc_download()
-        self.driver.get(server + "/home")
-        self._doc_delete()
 
-    def _doc_upload(self):
+        # import logging
+
+        # logger = logging.getLogger()
+        # logger.warn(sample_file)
+        # a = []
+        # print("a: " + a)
+
+        self.driver.get(server + "/home")
+        self._doc_upload(sample_doc)
+        time.sleep(50)
+        # assert self.driver.find_element_by_xpath("//*[@id='documentTableOnHomePage']/div[1]/table/tbody/tr/td[2]").is_displayed()
+        # self.driver.get(server + "/home")
+        # self._doc_download()
+        # self.driver.get(server + "/home")
+        # self._doc_delete()
+
+    def _doc_upload(self, sample_doc):
         f = self.driver.find_element_by_xpath("//*[@id='uploadForm1']/div[1]/input")
-        f.send_keys(os.getcwd() + "/assets/sample_image.png")
+        f.send_keys(sample_doc)
         self.driver.find_element_by_xpath("//*[@id='uploadForm1']").click()
         self.driver.find_element_by_xpath("//*[@id='uploadResume']").click()
         # time.sleep(5)
